924525763cd27155a5de60fae7114be871527168
Accro Crops <80210385+acccrop@users.noreply.github.com>
2021-04-19 22:26:33 +0530

    Update AMI_ID_Validation_Policy.py

--- AMI_ID_Validation_Policy.py
+++ AMI_ID_Validation_Policy.py
@@ -8,7 +8,7 @@ import json
 class CKV_AWS_999(BaseResourceCheck):
     def __init__(self):
         self.logger = logging.getLogger("{}".format(self.__module__))
-        name = "Ensure That AMI ID is ami-830c94e3"
+        name = "Ensure That Ec2 has atleast one or more tags"
         id = "CKV_AWS_999"
         supported_resources = '*'
         # CheckCategories are defined in models/enums.py
@@ -18,7 +18,7 @@ class CKV_AWS_999(BaseResourceCheck):
     def scan_resource_conf(self, conf):
         self.logger.debug("ssss" + json.dumps(conf))
         if 'tags' in conf.keys():
-            if conf['tags'][0]['env'] == "prod":
+            if len(conf['tags']) > 0::
                 return CheckResult.PASSED     
         return CheckResult.FAILED
 
